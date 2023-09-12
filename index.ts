4451592d8c86cadf5355861034f4e0ce3b8c75bd
Macumba45 <gonzalolovo@gmail.com>
2023-09-12 19:38:58 +0200

    cors

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 import axios from 'axios';
 const API_URL = "http://api.aviationstack.com/v1/flights?access_key=797372319f9cb0d9c22f18f276e23ac4"
 
 
 const app = express();
+app.use(cors());
 app.get('/', (req: any, res: any) => {
     res.send('Hello World!');
 });
