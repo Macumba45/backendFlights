5f98cc20836654b82d507a0b75960736a14e9f36
Macumba45 <gonzalolovo@gmail.com>
2023-09-12 22:03:54 +0200

    limit:10

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 import axios from 'axios';
-const API_URL = "http://api.aviationstack.com/v1/flights?access_key=797372319f9cb0d9c22f18f276e23ac4"
+const API_URL = "http://api.aviationstack.com/v1/flights?access_key=797372319f9cb0d9c22f18f276e23ac4&limit=10"
 
 
 const app = express();
