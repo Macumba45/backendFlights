4a677172bea6fc576ad3fe3b0402d481f9f42998
Macumba45 <gonzalolovo@gmail.com>
2023-09-12 19:35:40 +0200

    backend flights

---
+++ index.ts
@@ -0,0 +1,21 @@
+const express = require('express');
+import axios from 'axios';
+const API_URL = "http://api.aviationstack.com/v1/flights?access_key=797372319f9cb0d9c22f18f276e23ac4"
+
+
+const app = express();
+app.get('/', (req: any, res: any) => {
+    res.send('Hello World!');
+});
+
+
+app.get('/flights', async (req: any, res: any) => {
+    const response = await axios.get(API_URL)
+    res.send(response.data)
+});
+
+app.listen(3005, () => {
+    console.log('Example app listening on port 3005!');
+});
+
+
